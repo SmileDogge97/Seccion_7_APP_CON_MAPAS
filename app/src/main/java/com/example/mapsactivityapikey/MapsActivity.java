9aabe9751de2c85233bc7f55942fc3d50e1d779e
SmileDogge97 <Ing_JosFco_JarMtz_SD@outlook.es>
2022-11-22 18:14:29 -0600

    vamos a empezar S07-CL06: Mapa Street View

--- app/src/main/java/com/example/mapsactivityapikey/MapsActivity.java
+++ app/src/main/java/com/example/mapsactivityapikey/MapsActivity.java
@@ -47,5 +47,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
     }
 }
