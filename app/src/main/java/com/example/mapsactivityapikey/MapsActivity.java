a8f36a7c61ddfbb58d2ea40bd7ed84eac50d3d48
SmileDogge97 <Ing_JosFco_JarMtz_SD@outlook.es>
2022-11-28 14:24:22 -0600

    mapa con diseño personalizado

--- app/src/main/java/com/example/mapsactivityapikey/MapsActivity.java
+++ app/src/main/java/com/example/mapsactivityapikey/MapsActivity.java
@@ -2,29 +2,75 @@ package com.example.mapsactivityapikey;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
+
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
 import com.google.android.gms.maps.StreetViewPanorama;
 import com.google.android.gms.maps.StreetViewPanoramaFragment;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.SupportStreetViewPanoramaFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnStreetViewPanoramaReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    //esta variable es para el mapa normal
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
 
-        StreetViewPanoramaFragment streetViewPanoramaFragment =
+        //mapa de street view
+        /*StreetViewPanoramaFragment streetViewPanoramaFragment =
                 (StreetViewPanoramaFragment) getFragmentManager()
                         .findFragmentById(R.id.streetviewpanorama);
-        streetViewPanoramaFragment.getStreetViewPanoramaAsync(this);
+        streetViewPanoramaFragment.getStreetViewPanoramaAsync(this);*/
+
+        SupportMapFragment mapFragment = (SupportMapFragment)  getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        //aplicando estilo al mapa
+        try {
+            // Customise the styling of the base map using a JSON object defined
+            // in a raw resource file.
+            boolean success = mMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            this, R.raw.style_json));
+
+            if (!success) {
+                Log.e("TAG", "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e("TAG", "Can't find style. Error: ", e);
+        }
+        // Position the map's camera near Sydney, Australia.
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(-34, 151)));
+    }
+
+    //este método igual es de street view
+    /*@Override
     public void onStreetViewPanoramaReady(@NonNull StreetViewPanorama streetViewPanorama) {
         LatLng sanFrancisco = new LatLng(37.754130, -122.447129);
         streetViewPanorama.setPosition(sanFrancisco);
-    }
+    }*/
 }
